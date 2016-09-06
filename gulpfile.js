0c5c93d786a6d854e0ddfdf90fb1d5dbf872de01
AaronBP <aaron.phillips96@icloud.com>
2016-09-06 17:45:11 +0100

    Project ready for development

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+ 
+// ////////////////////////////
+// Require modules
+// ////////////////////////////
+
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jade = require('gulp-jade'),
+    fileInclude = require('gulp-file-include'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    sass = require('gulp-sass');
+ 
+// ////////////////////////////
+// Sass Tasks
+// ////////////////////////////
+
+gulp.task('sass', function(){
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(reload({stream: true}));
+});
+
+// ////////////////////////////
+// Jade Tasks
+// ////////////////////////////
+
+gulp.task('jade', function(){
+    return gulp.src('./jade/**/*.jade')
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./public/html'));
+
+    // gulp.src('./public/html/index.html')
+    // .pipe(fileInclude({
+    //     prefix: '@@',
+    //     basepath: '@file'
+    // }))
+    // .pipe(gulp.dest('./'))
+    // .pipe(reload({stream: true}));
+});
+
+// ////////////////////////////
+// Gulp File include
+// ////////////////////////////
+
+gulp.task('file-include', function(){
+    return gulp.src('./public/html/index.html')
+        .pipe(fileInclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(reload({stream: true}));
+});
+
+// ////////////////////////////
+// Browser Sync Tasks
+// ////////////////////////////
+
+gulp.task('browser-sync', function(){
+    browserSync({
+        server:{
+            baseDir: './'
+        }
+    });
+});
+
+// ////////////////////////////
+// Watch Tasks
+// ////////////////////////////
+
+gulp.task('watch', function(){
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./jade/**/*.jade', ['jade']);
+    gulp.watch('./public/html/**/*.html', ['file-include']);
+});
+
+// ////////////////////////////
+// Default Tasks
+// ////////////////////////////
+
+gulp.task('default', ['sass', 'jade', 'browser-sync', 'watch']);
